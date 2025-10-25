ea824c536fef52fc390b61b0f9c2dd29f02138e7
WWWSSSAAADDD <3781375775@qq.com>
2025-10-25 22:20:29 +0800

    commit for lost file

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,14 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    //protected访问权限：最多到 不同包的子类 可以访问，而不同包的无关类（如不同包的main类中，不可读取这个数据）
+    protected int fillCount;
+    protected int capacity;
+    public int capacity() {
+        return capacity;
+    }
+    public int fillCount() {
+        return fillCount;
+    }
+
+}
